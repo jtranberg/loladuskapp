4c7e9edeb7b01958a46c9c5c969512ea0ea3b53b
jaytranberg <jtranberg@hotmail.ca>
2024-09-30 23:21:19 -0700

    first

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,8 @@
 import React from "react";
 import "../styles/style.css";
-// import "../index"
 import foreground from "../img/opening2.jpg";
 import background from "../img/openingPage1.jpg";
 import pic1 from "../img/poster.png";
- import pic2 from "../../public/randomImages/13.jpg";
- import pic3 from "../../public/randomImages/24.jpg";
- import pic4 from "../../public/randomImages/default2.jpg";
 
 class Home extends React.Component {
   render() {
@@ -16,122 +12,39 @@ class Home extends React.Component {
 
         <div className="wrapper">
           <header>
-            <img className="background" id="pic" src={background} alt="backround" />
-
+            <img className="background" id="pic" src={background} alt="background" />
             <img className="foreground" id="picfore" src={foreground} alt="foreground" />
-            
           </header>
 
           <img className="pic1" src={pic1} alt="pic" />
-<h1 className="title">Welcome to Lola's Dusk</h1>
+          <h1 className="title">Welcome to Lola's Dusk</h1>
+
           <section className="section1">
-            Kerri Dawn brings to her readings more than 20 years' experience
-              with Tarot. With her extensive studies over the years, in the
-            Esoteric Arts; the occult, religion, astrology, numerology,
-            symbolism, energy healing she considers herself an intuitive
-            spiritual therapist. As well as a deep interest in alternative
-            natural health practices, such as herbal medicine, Ayurveda and
-            aromatherapy. Her sessions include an abundance of information,
-            addressing reality and the subconscious with her intuitional
-            abilities. She holds her 2nd level Reiki certification. Is an Essene
-            Minister (spiritual councillor) and a trained certified Clinical
-            Counselling Hypnotherapist. Kerri Dawn is an admitted scholar for
-            life, utilizing her connections to great spirit, provides answers
-            and wisdom for life's questions. Kerri also recently began her
-            degree in Metaphysics at Sedona University. Forever intrigued by the
-            mind, body, spirit, soul connection and utilizes all her studies in
-            her practice. Tarot reading sessions, in-store, on-line (zoom), or
-            by phone. Book in-store or contact me directly, for more
-            information.
+            Kerri Dawn brings to her readings more than 20 years' experience with Tarot.
+            With her extensive studies over the years in the Esoteric Arts, including
+            occult, religion, astrology, numerology, symbolism, energy healing, and more,
+            she considers herself an intuitive spiritual therapist...
           </section>
+
           <section className="section1">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Asperiores, illum modi velit debitis labore ducimus quae.
-            Consequatur velit cum modi suscipit? Ipsam ad accusamus error eum
-            nostrum corporis in libero id atque sunt et beatae officia, tenetur
-            optio dicta illum assumenda, incidunt iure enim explicabo itaque
-            vero. Quos sunt quam, ipsum assumenda facilis modi voluptatem natus
-            qui exercitationem minus, laudantium harum ea nihil, autem obcaecati
-            fuga! Quis, sit aut earum quod iure iusto vero, laboriosam aperiam
-            esse mollitia nisi voluptatem hic, omnis ipsa et! Consequuntur,
-            quibusdam, sed voluptates corporis eveniet provident ducimus
-            corrupti, veniam inventore reiciendis at quis deserunt beatae libero
-            illum! Voluptatem labore itaque molestias nobis repellendus
-            voluptate pariatur consequatur ea eos, voluptatibus distinctio natus
-            blanditiis tenetur amet nihil consequuntur exercitationem qui sequi
-            eaque repellat? Dicta cupiditate voluptatum unde velit, excepturi
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit...
           </section>
-          <img className="pic1" src={pic2} alt="pic" />
+
+          <img className="pic1" src="/randomImages/13.jpg" alt="pic" />
           <section className="section1">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Asperiores, illum modi velit debitis labore ducimus quae.
-            Consequatur velit cum modi suscipit? Ipsam ad accusamus error eum
-            nostrum corporis in libero id atque sunt et beatae officia, tenetur
-            optio dicta illum assumenda, incidunt iure enim explicabo itaque
-            vero. Quos sunt quam, ipsum assumenda facilis modi voluptatem natus
-            qui exercitationem minus, laudantium harum ea nihil, autem obcaecati
-            fuga! Quis, sit aut earum quod iure iusto vero, laboriosam aperiam
-            esse mollitia nisi voluptatem hic, omnis ipsa et! Consequuntur,
-            quibusdam, sed voluptates corporis eveniet provident ducimus
-            corrupti, veniam inventore reiciendis at quis deserunt beatae libero
-            illum! Voluptatem labore itaque molestias nobis repellendus
-            voluptate pariatur consequatur ea eos, voluptatibus distinctio natus
-            blanditiis tenetur amet nihil consequuntur exercitationem qui sequi
-            eaque repellat? Dicta cupiditate voluptatum unde velit, excepturi
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit...
           </section>
+
+          <img className="pic1" src="/randomImages/24.jpg" alt="pic" />
           <section className="section1">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Asperiores, illum modi velit debitis labore ducimus quae.
-            Consequatur velit cum modi suscipit? Ipsam ad accusamus error eum
-            nostrum corporis in libero id atque sunt et beatae officia, tenetur
-            optio dicta illum assumenda, incidunt iure enim explicabo itaque
-            vero. Quos sunt quam, ipsum assumenda facilis modi voluptatem natus
-            qui exercitationem minus, laudantium harum ea nihil, autem obcaecati
-            fuga! Quis, sit aut earum quod iure iusto vero, laboriosam aperiam
-            esse mollitia nisi voluptatem hic, omnis ipsa et! Consequuntur,
-            quibusdam, sed voluptates corporis eveniet provident ducimus
-            corrupti, veniam inventore reiciendis at quis deserunt beatae libero
-            illum! Voluptatem labore itaque molestias nobis repellendus
-            voluptate pariatur consequatur ea eos, voluptatibus distinctio natus
-            blanditiis tenetur amet nihil consequuntur exercitationem qui sequi
-            eaque repellat? Dicta cupiditate voluptatum unde velit, excepturi
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit...
           </section>
-          <img className="pic1" src={pic3} alt="pic" />
+
+          <img className="pic1" src="/randomImages/default2.jpg" alt="pic" />
           <section className="section1">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Asperiores, illum modi velit debitis labore ducimus quae.
-            Consequatur velit cum modi suscipit? Ipsam ad accusamus error eum
-            nostrum corporis in libero id atque sunt et beatae officia, tenetur
-            optio dicta illum assumenda, incidunt iure enim explicabo itaque
-            vero. Quos sunt quam, ipsum assumenda facilis modi voluptatem natus
-            qui exercitationem minus, laudantium harum ea nihil, autem obcaecati
-            fuga! Quis, sit aut earum quod iure iusto vero, laboriosam aperiam
-            esse mollitia nisi voluptatem hic, omnis ipsa et! Consequuntur,
-            quibusdam, sed voluptates corporis eveniet provident ducimus
-            corrupti, veniam inventore reiciendis at quis deserunt beatae libero
-            illum! Voluptatem labore itaque molestias nobis repellendus
-            voluptate pariatur consequatur ea eos, voluptatibus distinctio natus
-            blanditiis tenetur amet nihil consequuntur exercitationem qui sequi
-            eaque repellat? Dicta cupiditate voluptatum unde velit, excepturi
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit...
           </section>
-          <section className="section1">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Asperiores, illum modi velit debitis labore ducimus quae.
-            Consequatur velit cum modi suscipit? Ipsam ad accusamus error eum
-            nostrum corporis in libero id atque sunt et beatae officia, tenetur
-            optio dicta illum assumenda, incidunt iure enim explicabo itaque
-            vero. Quos sunt quam, ipsum assumenda facilis modi voluptatem natus
-            qui exercitationem minus, laudantium harum ea nihil, autem obcaecati
-            fuga! Quis, sit aut earum quod iure iusto vero, laboriosam aperiam
-            esse mollitia nisi voluptatem hic, omnis ipsa et! Consequuntur,
-            quibusdam, sed voluptates corporis eveniet provident ducimus
-            corrupti, veniam inventore reiciendis at quis deserunt beatae libero
-            illum! Voluptatem labore itaque molestias nobis repellendus
-            voluptate pariatur consequatur ea eos, voluptatibus distinctio natus
-            blanditiis tenetur amet nihil consequuntur exercitationem qui sequi
-            eaque repellat? Dicta cupiditate voluptatum unde velit, excepturi
-          </section>
-          <img className="pic1" src={pic4} alt="pic" />
+
           <div>
             <footer>Copyright@ 2023 Lola Dusk</footer>
           </div>
@@ -140,4 +53,5 @@ class Home extends React.Component {
     );
   }
 }
+
 export default Home;
